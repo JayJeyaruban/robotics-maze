f648bc00631467d7fe46ba6b1a4563c2b0265112
Jay Jeyaruban <jay.jeyaruban.15@ucl.ac.uk>
2016-05-19 13:29:52 +0100

    Updated map.h with sig for updateMaze.

--- map.h
+++ map.h
@@ -3,12 +3,7 @@
 
 // Default for dist needs to be set to 255 and visited = false
 typedef struct node {
-  // int dir[4];
   bool dir[4];
-  // int north;
-  // int south;
-  // int west;
-  // int east;
   int dist;
   bool visited;
 } Node;
@@ -21,5 +16,10 @@ Node maze[4][4];
  * the input node.
  */
 void updatePoint(int x, int y, bool north, bool east, bool south, bool west);
+
+/*
+ * Updates the dist values in the maze array when a coordinate is updated.
+ */
+void updateMaze(int x, int y);
 
 #endif /* MAP_H */
